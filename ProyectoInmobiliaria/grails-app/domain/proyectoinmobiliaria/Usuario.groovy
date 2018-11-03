b9f7970c477813e9edd219c184dc2625ff13e05f
juancdaneri <juancdaneri@github.com>
2018-11-02 21:56:41 -0300

    login parte 2

--- ProyectoInmobiliaria/grails-app/domain/proyectoinmobiliaria/Usuario.groovy
+++ ProyectoInmobiliaria/grails-app/domain/proyectoinmobiliaria/Usuario.groovy
@@ -9,45 +9,45 @@ class Usuario extends Cliente implements Serializable {
       String email
 
       def generateMD5_A(String s){
-          // return MessageDigest.getInstance("MD5").digest(s.bytes).encodeHex().toString()
+           return MessageDigest.getInstance("MD5").digest(s.bytes).encodeHex().toString()
       }
 
       boolean equals(otro) {
-        // is(otro) || (otro instanceof Usuario && otro.email == email)
+         is(otro) || (otro instanceof Usuario && otro.email == email)
       }
 
 
       String toString() {
-        // nombreUsuario
+         nombreUsuario
       }
 
       Set<Rol> getRoles() {
 
-      //               if (nombreUsuario!=null) {
-      //                   UsuarioRol.findAllByUsuario(this)*.rol
-      //               } else {
-      //                   new TreeSet<Rol>()
-      //               }
-      // }
+                     if (nombreUsuario!=null) {
+                         UsuarioRol.findAllByUsuario(this)*.rol
+                     } else {
+                         new TreeSet<Rol>()
+                     }
+       }
 
-      //     Set<UsuarioRol> getUsuarioRol() {
-      //   UsuarioRol.findAllByUsuario(this)
+           Set<UsuarioRol> getUsuarioRol() {
+         UsuarioRol.findAllByUsuario(this)
       }
 
       def beforeInsert() {
-        // password=generateMD5_A(password)
+         password=generateMD5_A(password)
 
       }
 
       def beforeUpdate() {
-        // if (isDirty('password')) {
-        //   password=generateMD5_A(password)
-        // }
+         if (isDirty('password')) {
+           password=generateMD5_A(password)
+         }
       }
 
             def beforeValidate() {
-                // nombreUsuario=nombreUsuario?.toUpperCase()
-                // email=email?.toLowerCase()
+                 nombreUsuario=nombreUsuario?.toUpperCase()
+                 email=email?.toLowerCase()
 
             }
 
--- ProyectoInmobiliaria/grails-app/init/proyectoinmobiliaria/BootStrap.groovy
+++ ProyectoInmobiliaria/grails-app/init/proyectoinmobiliaria/BootStrap.groovy
@@ -34,6 +34,38 @@ class BootStrap {
      //    String estado
 
 
+
+	 //Usuario
+
+	   /* def usuario1 = new Usuario(
+			nombre:"Juan",
+    		apellido:"Daneri",
+     		dni:4124214,
+     		direccion:"sdfsdf",
+     		telefono:1234567899,
+			nombreUsuario: "Juan",
+			password: "12345",
+			email: "juan@gmail.com")
+    	if(!usuario1.save(flush: true)) {
+			usuario1.errors.each{
+			println it
+      		}	
+    	}
+		def rol1 = new Rol(authority: "ADMINISTRADOR")
+		if(!rol1.save(flush: true)) {
+		rol1.errors.each{
+				println it
+			}
+		}
+
+		def usuarioRol1 = new UsuarioRol(usuario: usuario1, rol: rol1)
+    	if(!usuarioRol1.save(flush: true)) {
+    	usuarioRol1.errors.each{
+        		println it
+      		}
+    	}
+*/
+
     }
     def destroy = {
     }
